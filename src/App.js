595a133602287c912b72c4b14690da156ecceca3
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-02 15:07:13 -0600

    end of day some of fornt end done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [cars, setCars] = useState([])
+    const [newCarMake, setNewCarMake] = useState('')
+    const [newCarModel, setNewCarModel] = useState('')
+    const [newCarImg, setNewCarImg] = useState('')
+    const [newCarSold, setNewCarSold] = useState(false)
+
+    //handlers
+    const handleNewMakeChange = (event) => {
+        setNewCarMake(event.target.value);
+    }
+    const handleNewModelChange = (event) => {
+        setNewCarModel(event.target.value);
+    }
+    const handleNewImgChange = (event) => {
+        setNewCarImg(event.target.value);
+    }
+    const handleNewSoldChange = (event) => {
+        setNewCarSold(event.target.checked);
+    }
+    const handleNewCarFormSubmit = (event) => {
+        event.preventDefault()
+        axios.post(
+            'http://localhost:3000/cars',
+            {
+                make:newCarMake,
+                model:newCarModel,
+                img:newCarImg,
+                sold:newCarSold
+            }
+        )
+    }
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/cars')
+            .then((response) => {
+                setCars(response.data)
+            })
+    },[])
+
+    return (
+        <div>
+            <h1>Cars Index</h1>
+            <section>
+                <h2>Add New Car</h2>
+                <form onSubmit={handleNewCarFormSubmit}>
+                    Make: <input type='text' onChange={handleNewMakeChange}></input><br/>
+                    Model: <input type='text' onChange={handleNewModelChange}></input><br/>
+                    Image: <input type='text' onChange={handleNewImgChange}></input><br/>
+                    Sold: <input type='checkbox' onChange={handleNewSoldChange}></input><br/>
+                    <input type='submit' value='Submit'></input>
+                </form>
+            </section>
+            <section>
+                <h2>Cars</h2>
+                {
+                    cars.map((car)=>{
+                        return <div>
+                            <h4>{
+                                car.make
+                            }</h4>
+
+                        </div>
+                    })
+                }
+            </section>
+        </div>
+    )
 }
 
 export default App;
